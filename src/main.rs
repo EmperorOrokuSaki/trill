9af710453fefc1d8bb4974e13029e52fcbe0ed1e
EmperorOrokuSaki <artie.eth@gmail.com>
2024-02-25 12:23:01 +0400

    feat: add tokio, alloy

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,17 @@ mod app;
 mod error;
 mod tui;
 
+use alloy::{
+    primitives::{address, U256},
+    providers::{Provider, ProviderBuilder},
+};
 use color_eyre::{
     eyre::{bail, WrapErr},
     Result,
 };
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     error::install_hooks()?;
     let mut terminal = tui::init()?;
     App::default().run(&mut terminal)?;
